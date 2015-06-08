d84ad7615e0ed15e2ca6cc907fa9ad2ea4619e71
Mirko Mucaria <skogsfrae@gmail.com>
2015-06-08 16:07:46 +0200

    Sistemati errori nelle sys1 e sys2

--- source/exceptions.c
+++ source/exceptions.c
@@ -7,6 +7,27 @@ unsigned int free_pidmap = MAXPROC;
 pid_t last_pid = 0, last_freed_pid = 0;
 pcb_t *pidmap[MAXPROC];
 
+int get_pid_num(int pid){
+  int count;
+
+  if(pid == 0)
+    return 0;
+
+  while(pid > 0){
+    if(pid&1 == 0)
+      count++;
+    pid >>=1;
+  }
+
+  return count;
+}
+
+int get_pid_mask(int pid){
+  int mask = 1;
+
+  return mask <<= pid;
+}
+
 /* Bisogna aggiungere la priorita' */
 int SYSCALL(CREATEPROCESS, state_t *statep, priority_enum *prio)
 {
@@ -61,18 +82,18 @@ int SYSCALL(CREATEPROCESS, state_t *statep, priority_enum *prio)
 	  pid_bitmap ^= 1;
 	  tmp_bitmap = pid_bitmap ^ tmp_bitmap; /* extracting new pid */
 	  free_pidmap ^= tmp_bitmap;
-	  newp->pid = tmp_bitmap;
 	  last_pid = tmp_bitmap;
-	  pidmap[last_pid] = newp;
+	  newp->pid = get_pid_num(tmp_bitmap);
+	  pidmap[newp->pid - 1] = newp;
 	}
 
 	/* Riciclo pid */
 	else{
-	  newp->pid = last_freed_pid;
+	  newp->pid = get_pid_num(last_freed_pid);
 	  last_pid = last_freed_pid;
 	  free_pidmap ^= last_freed_pid;
 	  pid_bitmap ^= last_pid;
-	  pidmap[last_pid] = newp;
+	  pidmap[newp->pid] = newp;
 	  tmp_bitmap = pid_bitmap ^ (pid_bitmap >> 1); /* limito la ricerca*/
 	  while(i != tmp_bitmap){                      /* del nuovo freepid*/
 	    if(i & free_pidmap)
@@ -91,14 +112,16 @@ int SYSCALL(CREATEPROCESS, state_t *statep, priority_enum *prio)
 }
 
 pcb_t *removeChildren(pcb_t *parent){
+  int pidmask = get_pid_mask(parent->pid);
   while(emptyChild(parent))
     freePcb(removeChildren(parent->p_children));
   
-  if(parent->pid == last_pid)
+  if(pidmask == last_pid)
     last_pid = 0;
-  pid_bitmap ^= parent->pid;
-  free_pidmap ^= parent->pid;
-  last_freed_pid = parent->pid;
+  pid_bitmap ^= pidmask;
+  free_pidmap ^= pidmask;
+  last_freed_pid = pidmask;
+  pidmap[parent->pid] = NULL;
   
   return outChild(parent);
 }
@@ -106,24 +129,27 @@ pcb_t *removeChildren(pcb_t *parent){
 /* Bisogna valutare il caso che il processo sia in una coda di un semaforo */
 void SYSCALL(TEMINATEPROCESS, pid_t pid){
   pcb_t *parent, *child;
+  int pidmask = get_pid_mask(pid);
 
   parent = pidmap[pid];
+  outChild(parent);
   if(emptyChild(parent)){
-    if(pid == last_pid)
+    if(pidmask == last_pid)
       last_pid = 0;
-    pid_bitmap ^= pid;
-    free_pidmap ^= pid;
-    last_freed_pid = pid;
+    pid_bitmap ^= pidmask;
+    free_pidmap ^= pidmask;
+    last_freed_pid = pidmask;
     freePcb(parent);
   }
   else{
-    if(pid == last_pid)
+    if(pidmask == last_pid)
       last_pid = 0;
-    pid_bitmap ^= pid;
-    free_pidmap ^= pid;
-    last_freed_pid = pid;
+    pid_bitmap ^= pidmask;
+    free_pidmap ^= pidmask;
+    last_freed_pid = pidmask;
     freePcb(removeChildren(parent));
   }
+  pidmap[pid] = NULL;
 }
 
 void SYSCALL(VERHOGEN, int *semaddr, int weight){
